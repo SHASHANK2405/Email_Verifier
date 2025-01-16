fd8ff364c41b1f2cf19bc1359bd82d8f1f91b067
SHASHANK2405 <shashankgulati2405@gmail.com>
2025-01-17 01:30:22 +0530

    deployment 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //output: "export", // Keep this if you're using static export
   images: {
     unoptimized: true, // Optional: Only needed if you want unoptimized images
   },
