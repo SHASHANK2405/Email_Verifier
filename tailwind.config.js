8b17ae7b2625dc7e7889c6c1083f5644434fc54c
SHASHANK2405 <shashankgulati2405@gmail.com>
2025-01-16 21:42:39 +0530

    fifth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-import type { Config } from "tailwindcss";
-
-export default {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,4 +16,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
